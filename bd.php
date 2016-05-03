29fd8c319d092c04d56517d6b72d12feefec9eb5
Anderson Ferreira <andersonrans@hotmail.com>
2016-05-03 20:56:17 -0300

    login: admin senha : admin

--- bd.php
+++ bd.php
@@ -1,18 +1,3 @@
 <?php
-$valida[admin] = "acessorestrito";
-?><?php
-?><?php
-$valida[eu] = "eu";
-?><?php
-$valida[ele] = "ele";
-?><?php
-$valida[gg] = "gg";
-?><?php
-$valida[nn] = "nn";
-?><?php
-$valida[] = "";
-?><?php
-$valida[henry] = "1234";
-?><?php
-$valida[Anderson] = "28982890";
+$valida[admin] = "admin";
 ?>
